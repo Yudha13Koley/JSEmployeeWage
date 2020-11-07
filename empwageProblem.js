9937f658df29aa63e83f0412421712f984ce03c1
Yudha13Koley <yudhajit1998@gmail.com>
2020-11-07 08:01:21 +0530

    Refactored : Calculate employee daily hours by function based on absent,part time or full time

--- empwageProblem.js
+++ empwageProblem.js
@@ -1,13 +1,12 @@
-//UC2 calculates the daily wage
+//UC3 calculates the daily wage by function
 const IS_ABSENT=0;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_DURATION=4;
 const FULL_TIME_DURATION=8;
 const EMP_WAGE_PER_HOUR=20;
-empCheck=Math.floor(Math.random()*10)%3;
-let dailyHours=0;
-let dailyWage=0;
+function getDailyEmployeeHours(empCheck){
+    let dailyHours=0;
 switch(empCheck){
     case IS_ABSENT: dailyHours=0;
     break;
@@ -17,5 +16,9 @@ switch(empCheck){
     break;
     default: dailyHours=0; 
 }
-dailyWage=dailyHours*EMP_WAGE_PER_HOUR;
+return dailyHours;
+}
+empCheck=Math.floor(Math.random()*10)%3;
+let dailyHours=getDailyEmployeeHours(empCheck);
+let dailyWage=dailyHours*EMP_WAGE_PER_HOUR;
 console.log('Employee Daily Wage is : '+dailyWage);
